001236940c32fd7e54a9f19b17ffc909c72b4ff0
Vasudeva31 <149861784+Vasudeva31@users.noreply.github.com>
2025-06-02 13:27:56 +0530

    Add files via upload

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+
+const Contact = () => {
+    return (
+      <div className="py-10">
+        <h1 className="text-3xl font-semibold mb-4">Contact Us</h1>
+        <p className="text-gray-600">Feel free to reach out to us at: DVP@gmail.com</p>
+      </div>
+    );
+  };
+  
+  export default Contact;
+  
